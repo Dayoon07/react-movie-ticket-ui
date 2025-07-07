8bbc37cf457a98966dfebb9ba76ea3ad757dfb20
Dayoon07 <gangd0642@gmail.com>
2025-07-07 14:08:48 +0900

    .

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,1 @@
-export const REACT_APP_API_SERVER = `
-    https://d3ec-121-67-60-140.ngrok-free.app/movie/api
-`;
+export const REACT_APP_API_SERVER = "https://d3ec-121-67-60-140.ngrok-free.app/movie/api";
